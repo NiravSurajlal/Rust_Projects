4dd8f6ab10789266f68028604ff0db8b959eb218
Nirav <niravsurajlal@gmail.com>
2019-06-07 08:12:37 +0200

    starting 9

--- Chapter_6/ch6_01/target/debug/ch6_01.d
+++ Chapter_6/ch6_01/target/debug/ch6_01.d
@@ -1,1 +1,1 @@
-C:\Users\Nirav\Documents\Rust_Projects\Chapter_6\ch6_01\target\debug\ch6_01.pdb: C:\Users\Nirav\Documents\Rust_Projects\Chapter_6\ch6_01\src\main.rs
+/home/lab/Downloads/Nirav/Rust/Rust_Projects/Chapter_6/ch6_01/target/debug/ch6_01: /home/lab/Downloads/Nirav/Rust/Rust_Projects/Chapter_6/ch6_01/src/main.rs
---
+++ Chapter_6/ch6_01/target/debug/deps/ch6_01-24616147eaab9aff.d
@@ -0,0 +1,5 @@
+/home/lab/Downloads/Nirav/Rust/Rust_Projects/Chapter_6/ch6_01/target/debug/deps/ch6_01-24616147eaab9aff: src/main.rs
+
+/home/lab/Downloads/Nirav/Rust/Rust_Projects/Chapter_6/ch6_01/target/debug/deps/ch6_01-24616147eaab9aff.d: src/main.rs
+
+src/main.rs:
