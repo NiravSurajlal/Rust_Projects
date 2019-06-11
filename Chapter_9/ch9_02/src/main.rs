b6c36e4eec7d4e6fd7a94d6b346ad36758194634
Nirav <niravsurajlal@gmail.com>
2019-06-11 09:30:13 +0200

    ch9_02 process

---
+++ Chapter_9/ch9_02/src/main.rs
@@ -0,0 +1,8 @@
+use std::fs::File;
+
+fn main() {
+    // to ask type? Know not f32 so
+    let f: u32 = File::open("hello.txt");
+    // let f = File::open("hello.txt");
+
+}
---
+++ Chapter_9/ch9_02/target/debug/deps/ch9_02-765d395dc5d706c3.d
@@ -0,0 +1,5 @@
+/home/lab/Downloads/Nirav/Rust/Rust_Projects/Chapter_9/ch9_02/target/debug/deps/ch9_02-765d395dc5d706c3: src/main.rs
+
+/home/lab/Downloads/Nirav/Rust/Rust_Projects/Chapter_9/ch9_02/target/debug/deps/ch9_02-765d395dc5d706c3.d: src/main.rs
+
+src/main.rs:
