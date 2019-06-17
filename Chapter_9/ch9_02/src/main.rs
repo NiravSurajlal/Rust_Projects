3b494959cb0da61e4ea772dfde1c5c0ef5f8908c
Nirav <niravsurajlal@gmail.com>
2019-06-17 07:50:56 +0200

    Ch_02

--- Chapter_9/ch9_02/src/main.rs
+++ Chapter_9/ch9_02/src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     let f: u32 = File::open("hello.txt");
     // let f = File::open("hello.txt");
 
+    let f = match f {
+        // when file is okay, return inner file value of the ok variant & assign that file value to f 
+        Ok(file) => file,
+        // if there is no file (above) 
+        Err(error) => {panic!("Problem opening file", error),}
+    };
 }
